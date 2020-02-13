c5b5d226602fc93081f39710b52beed086477ad2
Дмитрий Дударев <44843835+DuIIDa@users.noreply.github.com>
2020-02-14 01:10:02 +0300

    Add files via upload

---
+++ Урок 16 дз2/main.js
@@ -0,0 +1,16 @@
+class First {
+    hello(){
+        console.log('Я метод родителя!');
+    }
+}
+
+class Second extends First{
+    hello(){
+        super.hello();
+        console.log('Я наследуемый метод!');
+    }
+}
+const two = new Second();
+
+
+two.hello();
